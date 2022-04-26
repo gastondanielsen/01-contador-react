e5c177f501a7bb4035898fac5f6d2288e75f7c72
Gaston Danielsen <gastondanielsen85@gmail.com>
2022-04-26 17:52:33 -0300

    Adding dist

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/01-contador-react/',
-  plugins: [react()]
-})
